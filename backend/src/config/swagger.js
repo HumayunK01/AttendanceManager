c0ad966f5da6d8b9ed8486f1a20ba31a06522f42
Humayun Khan <humayunk.pvt@gmail.com>
2025-12-27 02:53:05 +0530

    feat: Add Swagger configuration for backend API documentation.

--- backend/src/config/swagger.js
+++ backend/src/config/swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       { url: 'http://localhost:5000/api' },
-      { url: 'https://attendancemanager-6spn.onrender.com/' }
+      { url: 'https://attendancemanager-6spn.onrender.com/api' }
     ],
     components: {
       securitySchemes: {
